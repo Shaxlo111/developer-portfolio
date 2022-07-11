8146b2cf13beb05eea3296a06fa88f46588ac1da
Shaxlo111 <shaxloxonsobirovna@gmail.com>
2022-07-11 12:52:01 +0500

    portfolio is ready

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'shaxloxonsobirovna@gmail.com',
     phone: '+998 93 571-76-27',
-    address: '123, 53, apartment, st. Great Silk Road, Tashkent,',
+    address: '123, 53, apartment, st. Buyuk Ipak Yuli, Tashkent,',
 
     sheetAPI: ''
 }
